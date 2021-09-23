60d3f94bf9ad03de25438dd79a642be417dbc25c
Brett Johnson <brettthomasjohnson@gmail.com>
2021-09-23 14:29:43 -0700

    add raw and original data to repo. add event date to nutrients table

---
+++ scripts/nuts_o2_ERDDAP_data_wrangle.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(parsedate)
+library(googledrive)
+library(here)
+
+dates <- read_excel(here("original_data", "raw_data", "IYS_GoA_2019_Hydro&Chemistry_20190326.xlsx"), sheet = "Hydro_GoA") %>% 
+  select(Station, date) %>% 
+  distinct()
+
+chemistry <- read_excel(here("original_data", "raw_data", "IYS_GoA_2019_Hydro&Chemistry_20190326.xlsx"), sheet = "Chemistry_GoA")
+
+# Inlcude date in chemistry sheet
+chemistry <- left_join(dates, chemistry) %>% 
+  select(Station:"NO3  [mkM/l]")
+
+write_csv(chemistry, here("original_data", "IYS_2019_nutrients_O2.csv"))
