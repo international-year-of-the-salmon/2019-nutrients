2c24b34643800472c65b3d8dbc3a338f1bb4df37
Brett Johnson <brettthomasjohnson@gmail.com>
2022-02-28 10:49:52 -0800

    create profile_id for 2019 Nuts + Oxygen dataset

--- scripts/nuts_o2_ERDDAP_data_wrangle.R
+++ scripts/nuts_o2_ERDDAP_data_wrangle.R
@@ -5,9 +5,13 @@ library(parsedate)
 library(googledrive)
 library(here)
 
-dates <- read_excel(here("original_data", "raw_data", "IYS_GoA_2019_Hydro&Chemistry_20190326.xlsx"), sheet = "Hydro_GoA") %>% 
+dates <- read_excel(here("original_data", "raw_data", 
+                         "IYS_GoA_2019_Hydro&Chemistry_20190326.xlsx"),
+                    sheet = "Hydro_GoA") %>% 
   select(Station, date) %>% 
-  distinct()
+  distinct() %>% 
+  # No time available to group profiles by so create profile_id 
+  mutate(profile_id = paste0(date, ":Stn", 1:n(), ":profile", 1:n()))
 
 chemistry <- read_excel(here("original_data", "raw_data", "IYS_GoA_2019_Hydro&Chemistry_20190326.xlsx"), sheet = "Chemistry_GoA")
 
@@ -15,5 +19,6 @@ chemistry <- read_excel(here("original_data", "raw_data", "IYS_GoA_2019_Hydro&Ch
 chemistry <- left_join(dates, chemistry) %>% 
   select(Station:"NO3  [mkM/l]") %>% 
   mutate(Longitude = ifelse(Longitude > 180, -360 + Longitude, Longitude))
+
 
 write_csv(chemistry, here("original_data", "IYS_2019_nutrients_O2.csv"))
